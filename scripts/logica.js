4f7a46ce5a1d962c871f8f6638b7ce7f121d6cc0
Hunkillerx <jdiazseg9611@universidadean.edu.co>
2018-02-27 12:23:26 -0500

    cambios a texto con consola

--- scripts/logica.js
+++ scripts/logica.js
@@ -4,15 +4,15 @@ document.write("<h1> Hola mundo </h1>");
 
 var booleano = true;
 
-if(booleano){
+if (booleano) {
     alert("opcion verdadera");
-}else if(!booleano){
+} else if (!booleano) {
     alert("opcion falsa")
-}else{
+} else {
     alert("opcion alien")
 }
 numero = 1
-switch(numero){
+switch (numero) {
     case 1:
         alert("Te gustan los primos falsos");
         break;
@@ -30,22 +30,22 @@ switch(numero){
         break;
 }
 
-for(var i = 0; i<10;i++){
+for (var i = 0; i < 10; i++) {
     console.log(i);
 }
 var j = 0;
 
-while(j<10){
+while (j < 10) {
     console.log(j);
     j++;
 }
 
-var arreglo = new Array('ospina','arias','mina','sanchez','fabra','sanchez','aguilar','rodriguez','cuadrado','zapata','falcao');
+var arreglo = new Array('ospina', 'arias', 'mina', 'sanchez', 'fabra', 'sanchez', 'aguilar', 'rodriguez', 'cuadrado', 'zapata', 'falcao');
 
-for(var i = 0; i < arreglo.length;i++){
+for (var i = 0; i < arreglo.length; i++) {
     console.log(arreglo[i]);
 }
-while(arreglo.length > 0){
+while (arreglo.length > 0) {
     console.log(arreglo.pop());
 }
 
@@ -56,7 +56,7 @@ while(arreglo.length > 0){
  * @param   {[[Type]]} lado3 [[Description]]
  * @returns {[[Type]]} [[Description]]
  */
-function perimetroTriangulo(lado1,lado2,lado3){
+function perimetroTriangulo(lado1, lado2, lado3) {
     return lado1 + lado2 + lado3
 }
 
@@ -67,10 +67,21 @@ function perimetroTriangulo(lado1,lado2,lado3){
  * @param   {[[Type]]} lado3 [[Description]]
  * @returns {[[Type]]} [[Description]]
  */
-function semiperimetro(lado1,lado2,lado3){
-    return perimetroTriangulo(lado1,lado2,lado3) / 2;
+function semiperimetro(lado1, lado2, lado3) {
+    return perimetroTriangulo(lado1, lado2, lado3) / 2;
 }
 
-function areaTriangulo(){
-    return
+function areaTriangulo() {
+    return perimetroTriangulo * 2;
 }
+
+var elementoPorEtiqueta = document.getElementsByTagName("p");
+
+var elementoPorClase = document.getElementsByClassName('parrafo-class');
+
+var elementoPorID = document.getElementById('parrafo-id');
+
+var elementoNuevo = document.createElement("p","Soy un elemento creado");
+
+elementoNuevo.textContent = "Soy un elemento creado"
+elementoPorID.appendChild(elementoNuevo);
